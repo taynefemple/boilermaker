472ddf605fe23602885b01fd76dca27bb5d3684d
Wayne Temple <waynetemple@gmail.com>
2018-02-23 14:07:05 -0600

    webpack config complete-- workingon frontend

--- server/api/apiIndex.js
+++ server/api/apiIndex.js
@@ -1,11 +1,11 @@
-const router = require(express).Router;
+const router = require('express').Router;
 const stuff = require('./stuff')
 
 router.use('/stuff', stuff);
 
 router.use(function (req, res, next) {
   const err = new Error('not found');
-  error.status = 404;
+  err.status = 404;
   next(err);
 })
 
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  entry: './client/index.jsx',
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /(node_modules | bower_components)/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['react']
+        }
+      }]
+  }
+}
